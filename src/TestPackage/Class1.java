d36bab0273245ce545ee11d79a776fcdb3d03720
Bhagyashree Yeole <1991shirude@gmail.com>
2021-08-31 23:31:18 +0530

    Bhagyashree Test

---
+++ src/TestPackage/Class1.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Test class 1");
+
+	}
+
+}
---
+++ src/TestPackage/Class2.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test class 2");
+
+	}
+
+}
